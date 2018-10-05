c7f7a80720822eed6fa3bd3f81c7f6d961ac9e76
Pierre-Luc Bruyere <proteama@yahoo.com>
2018-10-05 14:59:50 -0400

    Initial code

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Pierre-Luc Bruyere on 2018-10-05.
+//  Copyright © 2018 Pierre-Luc Bruyere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
